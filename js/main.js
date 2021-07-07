6e7c5763cb902af7e18c6dfb2b4e5cbdc690627a
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-07-08 00:13:39 +0200

    create database form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-// ================= Set Input Reservation Date ===============
+//================== Form Sign Up ==============================
 
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+
+//---------------------- END -----------------------------------
+
+// ================= Set Input Reservation Date ===============
+/*
 const setDateInputReservation = () => {
     var today = new Date().toISOString().split('T')[0];
     var nextWeekDate = new Date(new Date().getTime() + 14 * 24 * 60 * 60 * 1000).toISOString().split('T')[0]
@@ -69,7 +95,7 @@ document.querySelectorAll('.btnTimeReserve').forEach(btn => {
         console.log(btn.getAttribute('time'))
     })
 })
-
+*/
 // document.getElementById('datefield').addEventListener('change', () => {
 //     let date = document.getElementById('datefield').value
 //     console.log(date)
