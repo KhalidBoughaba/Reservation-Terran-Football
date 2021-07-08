aaf908b19189cb4a291c4c877da76902a6bf559b
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-07-08 13:30:15 +0100

    connect form with database

--- js/main.js
+++ js/main.js
@@ -2,23 +2,55 @@
 
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function() {
-  'use strict';
-  window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+                document.querySelector(".submit-signup").addEventListener('submit', (e) => {
+                    e.preventDefault();
+
+                    let prenom = document.querySelector(".prenom").value,
+                        nom = document.querySelector(".nom").value,
+                        password = document.querySelector(".pass").value,
+                        phone = document.querySelector(".tele").value;
+
+                    console.log(prenom, nom, password, phone)
+
+                    $.ajax({
+                        url: 'php/connect.php',
+                        type: 'POST',
+                        data: {
+                            prenom: prenom,
+                            nom: nom,
+                            password: password,
+                            phone: phone
+                        },
+                        success: () => {
+                            document.querySelector('.close').click()
+                            $('#registre').get(0).reset()
+                            $('#registre').removeClass('was-validated')
+                        }
+
+                    })
+
+                })
+            }, false);
+        });
+    }, false);
 })();
+
+document.getElementById('btnOpen').onclick = () => {
+    $('#registre').removeClass('was-validated')
+}
 
 
 
@@ -95,11 +127,11 @@ document.querySelectorAll('.btnTimeReserve').forEach(btn => {
         console.log(btn.getAttribute('time'))
     })
 })
-*/
+
 // document.getElementById('datefield').addEventListener('change', () => {
 //     let date = document.getElementById('datefield').value
 //     console.log(date)
 // })
-
+ */
 
 // -------------------------- End ---------------------
