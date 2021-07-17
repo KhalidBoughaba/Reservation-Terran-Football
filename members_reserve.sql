b7ed44028f54d04827f368690fc5f5a7d4566e92
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-07-17 16:13:00 +0100

    modified on crud

--- members_reserve.sql
+++ members_reserve.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 16, 2021 at 05:51 PM
+-- Generation Time: Jul 13, 2021 at 05:58 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -24,25 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `member`
 --
 
@@ -50,62 +31,20 @@ CREATE TABLE `member` (
   `id` int(11) NOT NULL,
   `Firstname` varchar(50) NOT NULL,
   `Lastname` varchar(50) NOT NULL,
-  `Phone` int(10) NOT NULL,
-  `Hours` int(2) NOT NULL,
-  `Date` date NOT NULL,
-  `status` int(1) NOT NULL
+  `phone` bigint(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`id`, `Firstname`, `Lastname`, `Phone`, `Hours`, `Date`, `status`) VALUES
-(23, 'omar', 'elytimm', 877676234, 8, '2021-07-15', 0),
-(26, 'jhjh', 'jhjh', 0, 9, '2021-07-15', 0),
-(27, 'jhj', 'hjhjhJHJ', 67676, 9, '2021-07-15', 0),
-(28, 'UHU', 'Boughaba', 111111111, 15, '2021-07-29', 0),
-(29, 'abdo', 'morhit', 2147483647, 11, '2021-07-28', 0),
-(30, 'ismail', 'Boughaba', 987654321, 17, '2021-07-29', 0),
-(31, 'UHU', 'Morig', 877676234, 18, '2021-07-19', 0),
-(32, 'Khalid', '23456', 877676, 8, '2021-07-15', 0),
-(33, 'UHU', 'Boughaba', 111111111, 8, '2021-07-29', 0),
-(34, 'hghg', 'khalid', 877676234, 8, '2021-07-15', 0),
-(35, 'jhjhj', 'hjh', 987654321, 8, '2021-07-15', 0),
-(36, 'hghg', 'hghg', 877676234, 8, '2021-07-15', 0),
-(37, 'hgh', 'ghghg', 877676, 12, '2021-07-15', 0),
-(38, 'Khalid', 'Morig', 654378976, 8, '2021-07-27', 0),
-(40, 'omar', 'Ajbar', 638712392, 0, '2021-07-30', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `status`
---
-
-CREATE TABLE `status` (
-  `id` int(11) NOT NULL,
-  `status` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `status`
---
-
-INSERT INTO `status` (`id`, `status`) VALUES
-(1, 0),
-(2, 0),
-(3, 0);
+INSERT INTO `member` (`id`, `Firstname`, `Lastname`, `phone`) VALUES
+(3, '', '', 0),
+(5, 'Khalid', 'Boughaba', 987654321);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `member`
@@ -114,32 +53,14 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
-
---
--- AUTO_INCREMENT for table `status`
---
-ALTER TABLE `status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
