2889c21ea492454b9ab2e2f613cf1b385c0f8dec
mourigdev <mourig.abdellah.dev@gmial.com>
2021-07-20 17:07:20 +0100

    add SQL

--- members_reserve.sql
+++ members_reserve.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2021 at 08:14 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Jul 20, 2021 at 06:05 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,60 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Table structure for table `member`
 --
 
-CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reservation`
---
-
-CREATE TABLE `reservation` (
+CREATE TABLE `member` (
   `id` int(11) NOT NULL,
   `Firstname` varchar(50) NOT NULL,
   `Lastname` varchar(50) NOT NULL,
-  `Phone` int(10) NOT NULL,
-  `Hours` varchar(11) NOT NULL,
-  `Date` varchar(50) NOT NULL
+  `Phone` bigint(10) NOT NULL,
+  `Date` varchar(255) NOT NULL,
+  `Hours` int(2) NOT NULL,
+  `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `reservation`
+-- Dumping data for table `member`
 --
 
-INSERT INTO `reservation` (`id`, `Firstname`, `Lastname`, `Phone`, `Hours`, `Date`) VALUES
-(1, 'khalid', 'boughaba', 141785234, '18', '01-01-2021'),
-(2, 'abdellah', 'morig', 321655894, '17', '02-02-2021'),
-(3, 'omar', 'ajbar', 741852661, '17', '2021-07-19');
+INSERT INTO `member` (`id`, `Firstname`, `Lastname`, `Phone`, `Date`, `Hours`, `status`) VALUES
+(6, 'Abdellah', 'Mourig', 212630334008, '2021-07-19', 10, 0),
+(7, 'hghg', 'hghg', 87878778, '2021-07-19', 11, 1),
+(8, 'Abdellah', 'Mourig', 212630334008, '2021-07-22', 11, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `member`
 --
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `reservation`
---
-ALTER TABLE `reservation`
+ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -85,16 +61,10 @@ ALTER TABLE `reservation`
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `member`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
