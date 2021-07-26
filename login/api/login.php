7d0cb21d8ba24d6a27bea5213e9e591a599a3408
KhalidBoughaba <boughaba.khalid.dev@gmail.com>
2021-07-26 16:47:51 +0100

    modified on page reservation

--- login/api/login.php
+++ login/api/login.php
@@ -17,7 +17,7 @@ $result = mysqli_query($connect,$sql);
 if(mysqli_num_rows($result) == 1){
   session_start();
   $_SESSION['login'] = "loginTrue";
-  header("location:http://localhost/GitHub/Reservation-Terran-Football/crud/");
+  header("location:http://localhost/ReserFoot/crud/index.html");
 }else{
   header("location:../");
 }
